07ca7fe10da494ff4fb86a5143abfea15227a34a
Oumnia RIFAA <rifaaoumnia@gmail.com>
2024-04-02 00:29:05 +0000

    Commit hospital2

---
+++ README.md
@@ -0,0 +1,64 @@
+<h3>Compte Rendu 2ème Avtivité (PATIENT-MEDECIN)<h3>
+<h4>1-Creation de l'entité Patient</h4>
+<img src="Captures/Patient.png">
+<h4>2-Creation de l'entité Medecin</h4>
+<img src="Captures/Medecin.png">
+<h4>3-Creation de l'entité Consultation</h4>
+<img src="Captures/Consultation.png">
+<h4>4-Creation de l'entité RDV</h4>
+<img src="Captures/RDV.png">
+<h4>5-Creation de l'entité StatusRDV</h4>
+<img src="Captures/Status.png">
+<h4>6-Creation de Repository Patient</h4>
+<img src="Captures/PatientRepo.png">
+<h4>7-Creation de Repository Medecin</h4>
+<img src="Captures/MedRepo.png">
+<h4>8-Creation de Repository Consultation</h4>
+<img src="Captures/ConsultatioRepo.png">
+<h4>9-Creation de Repository RDV</h4>
+<img src="Captures/RDVRepo.png">
+<h4>10-Configuration de fichier application.properties</h4>
+<img src="Captures/app.prop.png">
+<h4>11-Base de données H2</h4>
+<img src="Captures/h2.png">
+<h4>12-Opérations de gestion </h4>
+<h5>-----Ajouter-----</h5>
+<h5>-----PATIENT-----</h5>
+<img src="Captures/AjoutPatient.png">
+<h5>-----MEDECIN-----</h5>
+<img src="Captures/AjoutMedecin.png">
+<h5>-----CONSULTATION-----</h5>
+<img src="Captures/AjouterConsultation.png">
+<h5>-----RDV-----</h5>
+<img src="Captures/AjouterRDV.png">
+<h5>-----Chercher-----</h5>
+<h5>-----PATIENT-----</h5>
+<img src="Captures/ChercherPatient.png">
+<h5>-----MEDECIN-----</h5>
+<img src="Captures/ChercherMedecin.png">
+<h4>13- Creation de l'interface IHospitalService</h4>
+<img src="Captures/ihospital.png">
+<h4>14- L'implementation de  l'interface IHospitalService</h4>
+<img src="Captures/impl1.png">
+<img src="Captures/impl2.png">
+<img src="Captures/impl3.png">
+<h4>15- Opérations de gestion</h4>
+<h5>-----PATIENT-----</h5>
+<img src="Captures/pat.png">
+<h5>-----MEDECIN-----</h5>
+<img src="Captures/med.png">
+<h5>-----------------------------</h5>
+<img src="Captures/ChercherPat-Med.png">
+<h5>-----CONSULTATION-----</h5>
+<img src="Captures/consu.png">
+<h5>-----RDV-----</h5>
+<img src="Captures/rdv1.png">
+<h5>-----BD-----</h5>
+<h5>-----PATIENT-----</h5>
+<img src="Captures/PatientBD.png">
+<h5>-----MEDECIN-----</h5>
+<img src="Captures/MedecinBD2.png">
+<h5>-----CONSULTATION-----</h5>
+<img src="Captures/ConsultBD.png">
+<h5>-----RDV-----</h5>
+<img src="Captures/RDVBD.png">
--- src/main/java/ma/tpsJ2EE/hospital/service/HospitalServiceImpl.java
+++ src/main/java/ma/tpsJ2EE/hospital/service/HospitalServiceImpl.java
@@ -1,5 +1,4 @@
 package ma.tpsJ2EE.hospital.service;
-
 import jakarta.transaction.Transactional;
 import ma.tpsJ2EE.hospital.entities.Consultation;
 import ma.tpsJ2EE.hospital.entities.Medecin;
@@ -11,68 +10,43 @@ import ma.tpsJ2EE.hospital.repositories.PatientRepository;
 import ma.tpsJ2EE.hospital.repositories.RendezVousRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 @Service
 @Transactional
 public class HospitalServiceImpl implements IHospitalService {
-
     private PatientRepository patientRepository;
     private MedecinRepository medecinRepository;
     private RendezVousRepository rendezVousRepository;
     private ConsultationRepository consultationRepository;
-
     @Autowired
     public HospitalServiceImpl(PatientRepository patientRepository, MedecinRepository medecinRepository,
                                RendezVousRepository rendezVousRepository, ConsultationRepository consultationRepository) {
         this.patientRepository = patientRepository;
         this.medecinRepository = medecinRepository;
         this.rendezVousRepository = rendezVousRepository;
-        this.consultationRepository = consultationRepository;
-    }
-
+        this.consultationRepository = consultationRepository;}
     @Override
-    public Patient savePatient(Patient patient) {
-        return patientRepository.save(patient);
-    }
-
+    public Patient savePatient(Patient patient) {return patientRepository.save(patient);}
     @Override
-    public Medecin saveMedecin(Medecin medecin) {
-        return medecinRepository.save(medecin);
-    }
-
+    public Medecin saveMedecin(Medecin medecin) {return medecinRepository.save(medecin);}
     @Override
-    public RendezVous saveRDV(RendezVous rendezVous) {
-        return rendezVousRepository.save(rendezVous);
-    }
-
+    public RendezVous saveRDV(RendezVous rendezVous) {return rendezVousRepository.save(rendezVous);}
     @Override
-    public Consultation saveConsultation(Consultation consultation) {
-        return consultationRepository.save(consultation);
-    }
-
+    public Consultation saveConsultation(Consultation consultation) {return consultationRepository.save(consultation);}
     @Override
-    public Patient findbynom(String name) {
-        return null;
-    }
-
+    public Patient findbynom(String name) {return null;}
     @Override
     public Medecin findBynom(String name) {
         return null;
     }
-
     @Override
     public RendezVous findbyid(Long id) {
         return rendezVousRepository.findById(id).orElse(null);
     }
-
     @Override
     public Patient findByid(Long id) {
         return patientRepository.findById(id).orElse(null);
     }
-
     @Override
-    public Medecin FinfById(Long id) {
-        return medecinRepository.findById(id).orElse(null);
-    }
+    public Medecin FinfById(Long id) {return medecinRepository.findById(id).orElse(null);}
 
 }
